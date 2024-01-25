328151983a0d1982678a7e7ee4e8af5f12fb440c
jjanggu1 <sdj970514@naver.com>
2024-01-25 15:00:36 +0900

    TodosContainer 리팩토링(connect => useSelector&useDisPatch로 변환)

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,43 +1,43 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { changeInput, add, remove } from "../modules/todos";
 import AddTask from "../components/Main/AddTask/AddTask";
 import TodoList from "../components/Main/TodoList/TodoList";
-import Main from "../components/Main/Main";
 
-const TodosContainer = ({
-    input,
-    todos,
-    changeInput,
-    add,
-    remove
-}) => {
+const TodosContainer = () => {
+    const { input, todos } = useSelector(state => ({
+        input: state.todos.input,
+        todos: state.todos.todos,
+    }));
+
+    const dispatch = useDispatch();
+    const onChangeInput = useCallback(
+        input => dispatch(changeInput(input)),
+        [dispatch]
+    );
+    const onAdd = useCallback(
+        text => dispatch(add(text)),
+        [dispatch]
+    );
+    const onRemove = useCallback(
+        id => dispatch(remove(id)),
+        [dispatch]
+    );
+
     return (
         <>
             <AddTask
                 input={input}
-                onChangeInput={changeInput}
-                onAdd={add}
+                onChangeInput={onChangeInput}
+                onAdd={onAdd}
                 
             />
             <TodoList 
                 todos={todos}
-                onRemove={remove}
+                onRemove={onRemove}
             />
         </>
     );
 };
 
-export default connect(
-    // 비구조화 할당을 통해 todos를 분리하며
-    // state.todos.input 대신 todos.input을 사용
-    ({ todos }) => ({
-        input: todos.input,
-        todos: todos.todos,
-    }),
-    {
-        changeInput,
-        add,
-        remove
-    },
-)(TodosContainer);
+export default TodosContainer;
